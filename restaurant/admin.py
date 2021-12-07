ad6c3b760049c35012b8bfa786c5f764020a56ce
HeleJ <hele.juhanson@gmail.com>
2021-12-07 21:36:55 +0000

    Change meals model to restaurant

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -3,6 +3,5 @@ Superuser models
 """
 
 from django.contrib import admin
-from .models import Meals, Booking
 
 # Register your models here.
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Meals(models.Model):
+class restaurant(models.Model):
     """
     Meals details for Customer
     """
@@ -13,18 +12,3 @@ class Meals(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     preparation_time = models.IntegerField()
     image = models.ImageField(upload_to='media/', default=True)
-
-    def save(self, *args, **kwargs):
-        if not self.slug and self.name:
-            self.slug = slugify(self.name)
-        super(Meals, self).save(*args, **kwargs)
-
-    class Meta:
-        """
-        Class Meta
-        """
-        verbose_name = 'meal'
-        verbose_name_plural = 'meals'
-
-    def __str__(self):
-        return str(self.name)
